066d6276518416addf7bacfe95b66b9315dcedbb
nathan <nathanberry97@live.com>
2025-02-05 17:36:01 +0000

    renaming html templates to tmpl instead of html

--- internal/routes/initialiseRouter.go
+++ internal/routes/initialiseRouter.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func InitialiseRouter() *gin.Engine {
 	router := gin.Default()
 	router.Static("/static", "./static")
-	router.LoadHTMLGlob("templates/*.html")
+	router.LoadHTMLGlob("templates/*.tmpl")
 
 	// Health check
 	healthCheck(router)
