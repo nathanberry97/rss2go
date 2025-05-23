9422afff8bb320fb236e0d406e68058d28816455
Nathan <nathanberry97@live.com>
2025-05-23 19:31:29 +0100

    refactor project - fixed pag bug, latest only returns 1 month of content, and the pub date is more readable to when the article was published

---
+++ internal/services/utils.go
@@ -0,0 +1,46 @@
+package services
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/nathanberry97/rss2go/internal/schema"
+	"github.com/nathanberry97/rss2go/internal/utils"
+)
+
+func GetArticleName(conn *sql.DB, id string) (string, error) {
+	query := "SELECT name FROM feeds WHERE id = ?"
+
+	var name string
+	if err := conn.QueryRow(query, id).Scan(&name); err != nil {
+		return "", fmt.Errorf("failed to get feed name for feed id: %w", err)
+	}
+
+	return name, nil
+}
+
+func formatArticles(rows *sql.Rows) ([]schema.RssArticle, error) {
+	var articles []schema.RssArticle
+
+	for rows.Next() {
+		var article schema.RssArticle
+		if err := rows.Scan(&article.FeedName, &article.FeedId, &article.Id, &article.Title, &article.Link, &article.PubDate, &article.Fav, &article.Later); err != nil {
+			return []schema.RssArticle{}, fmt.Errorf("failed to scan row: %w", err)
+		}
+
+		formatPubDate, err := utils.FormatTime(article.PubDate, time.RFC3339)
+		if err != nil {
+			return []schema.RssArticle{}, fmt.Errorf("failed parsing pubDate: %w", err)
+		}
+
+		article.PubDate = formatPubDate
+		articles = append(articles, article)
+	}
+
+	if err := rows.Err(); err != nil {
+		return []schema.RssArticle{}, fmt.Errorf("rows iteration error: %w", err)
+	}
+
+	return articles, nil
+}
