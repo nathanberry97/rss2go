4b1009cfb239defb1ce0b697cbd182501b82147e
Nathan Berry <54477304+nathanberry97@users.noreply.github.com>
2025-05-26 22:19:50 +0100

    add containerfile which is will be used to build the application and … (#22)
    * add containerfile which is will be used to build the application and allow it to be ran on other machines
    
    * update comments within containerfile

--- internal/utils/index.go
+++ internal/utils/index.go
@@ -1,11 +1,7 @@
 package utils
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strings"
 	"time"
 )
 
@@ -60,24 +56,5 @@ func FormatTime(dateStr, inputFormat string) (string, error) {
 			return "1 year ago", nil
 		}
 		return fmt.Sprintf("%d years ago", years), nil
-	}
-}
-
-func SetEnv(envFile string) {
-	file, err := os.Open(envFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		env := strings.Split(scanner.Text(), "=")
-
-		if strings.Contains(env[1], "'") {
-			env[1] = strings.Replace(env[1], "'", "", -1)
-		}
-
-		os.Setenv(env[0], env[1])
 	}
 }
